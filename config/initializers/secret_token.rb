03cb19fe09cd42fda154aa68fb3bde9c0fb23fda
Reiner Lim <reinerlim17@gmail.com>
2012-08-23 16:44:06 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kyokushin::Application.config.secret_token = '2c7317f4c4df4bdf91486b82c2328d7622fd150bca8e2b8be33c64dbb5df936d6c4f688b0bb759b5ed691e8afff9d5b95b8e78aa7b292787581f7b6b8e672d1f'
