f3dcd71bdb4db76dbb72b7fcd7ff64535d438f77
Reiner Lim <reinerlim17@gmail.com>
2012-08-25 15:25:23 +0800

    done fixing the problem in layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Kyokushin::Application.routes.draw do
   resources :users
-
+ # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
  root :to => "pages#home"
  
 match '/signup', to: 'users#new'
